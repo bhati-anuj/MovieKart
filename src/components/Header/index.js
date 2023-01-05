c8cf569506cb87dea868edd7e8fcef5f91decdb6
Anuj <bhatianuj7247@gmail.com>
2023-01-06 00:41:57 +0530

    Initial commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,35 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+
+import MKLogo from "../../images/White_logo.svg";
+
+import TMDBLogo from "../../images/tmdb_logo.svg";
+
+import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
+//Context
+import { Context } from "../../context";
+
+const Header = () => {
+  const [user] = useContext(Context);
+  console.log(user);
+
+  return (
+    <Wrapper>
+      <Content>
+        <Link to="/">
+          <LogoImg src={MKLogo} alt="movieKart-logo" />
+        </Link>
+        {user ? (
+          <span>Logged in as: {user.username}</span>
+        ) : (
+          <Link to="/login">
+            <span>Log in</span>
+          </Link>
+        )}
+       {/* <TMDBLogoImg src={TMDBLogo} alt="tmdb-logo" /> */}
+      </Content>
+    </Wrapper>
+  );
+};
+
+export default Header;
